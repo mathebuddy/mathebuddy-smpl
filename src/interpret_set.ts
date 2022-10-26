3e9180a7bba6c3dc33df66a8d7723a8db6c329e2
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2022-10-26 17:38:24 +0200

    Create interpret_set.ts

---
+++ src/interpret_set.ts
@@ -0,0 +1,26 @@
+/**
+ * mathe:buddy - eine gamifizierte Lern-App für die Hoehere Mathematik
+ * (c) 2022 by TH Koeln
+ * Author: Andreas Schwenk contact@compiler-construction.com
+ * Funded by: FREIRAUM 2022, Stiftung Innovation in der Hochschullehre
+ * License: GPL-3.0-or-later
+ */
+
+import { RunError, SMPL_Interpreter } from './interpret';
+
+import { Set_INT } from './set';
+
+export class SMPL_Interpreter_Set {
+  private parent: SMPL_Interpreter = null;
+
+  constructor(parent: SMPL_Interpreter) {
+    this.parent = parent;
+  }
+
+  //G set(elements:INT_LIST): SET_INT -> _createSet;
+  _createSet(elements: number[]): Set_INT {
+    const s = new Set_INT();
+    s.elements = elements;
+    return s;
+  }
+}
