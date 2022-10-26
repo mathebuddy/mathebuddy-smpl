3f161b6a17567eb8d16c020b5af3fb715c73d17d
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2022-10-26 17:38:19 +0200

    Update interpret.ts

--- src/interpret.ts
+++ src/interpret.ts
@@ -17,6 +17,7 @@ import { SMPL_Interpreter_Matrix } from './interpret_matrix';
 import { SMPL_Interpreter_Complex } from './interpret_complex';
 import { SMPL_Interpreter_Basic } from './interpret_basic';
 import { SMPL_Interpreter_Term } from './interpret_term';
+import { SMPL_Interpreter_Set } from './interpret_set';
 import { Set_INT } from './set';
 
 export class RunError extends Error {
@@ -31,12 +32,14 @@ export class SMPL_Interpreter {
   interpret_complex: SMPL_Interpreter_Complex = null;
   interpret_basic: SMPL_Interpreter_Basic = null;
   interpret_term: SMPL_Interpreter_Term = null;
+  interpret_set: SMPL_Interpreter_Set = null;
 
   public constructor() {
     this.interpret_matrix = new SMPL_Interpreter_Matrix(this);
     this.interpret_complex = new SMPL_Interpreter_Complex(this);
     this.interpret_basic = new SMPL_Interpreter_Basic(this);
     this.interpret_term = new SMPL_Interpreter_Term(this);
+    this.interpret_set = new SMPL_Interpreter_Set(this);
   }
 
   public interpret(code: string, locals: SymTabEntry[]): void {
