1f6e15e9cd5755950dec3ceb5bb8cf95d9f64cef
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2022-11-28 12:00:09 +0100

    Update interpret_complex.ts

--- src/interpret_complex.ts
+++ src/interpret_complex.ts
@@ -18,9 +18,25 @@ export class SMPL_Interpreter_Complex {
     this.parent = parent;
   }
 
+  _create(real: number, imag: number): Complex {
+    return new Complex(real, imag);
+  }
+
   //G _add(x:COMPLEX,y:COMPLEX):COMPLEX -> _addComplex;
   _addComplex(x: Complex, y: Complex): Complex {
     return Complex.mathjs2complex(mathjs.add(x.toMathJs(), y.toMathJs()));
+  }
+
+  //G _add(x:INT, y:COMPLEX): COMPLEX -> _addNumberComplex;
+  //G _add(x:REAL, y:COMPLEX): COMPLEX -> _addNumberComplex;
+  _addNumberComplex(x: number, y: Complex): Complex {
+    return new Complex(x + y.real, y.imag);
+  }
+
+  //G _add(x:COMPLEX, y:INT): COMPLEX -> _addComplexNumber;
+  //G _add(x:COMPLEX, y:REAL): COMPLEX -> _addComplexNumber;
+  _addComplexNumber(x: Complex, y: number): Complex {
+    return new Complex(x.real + y, x.imag);
   }
 
   //G _sub(x:COMPLEX,y:COMPLEX):COMPLEX -> _subComplex;
@@ -28,15 +44,113 @@ export class SMPL_Interpreter_Complex {
     return Complex.mathjs2complex(mathjs.subtract(x.toMathJs(), y.toMathJs()));
   }
 
-  //G exp(x:COMPLEX):COMPLEX -> _expComplex;
-  // TODO
+  //G _sub(x:INT, y:COMPLEX): COMPLEX -> _subNumberComplex;
+  //G _sub(x:REAL, y:COMPLEX): COMPLEX -> _subNumberComplex;
+  _subNumberComplex(x: number, y: Complex): Complex {
+    return new Complex(x - y.real, y.imag);
+  }
 
-  //G sqrt(x:COMPLEX): COMPLEX -> _sqrtComplex;
-  // TODO
+  //G _sub(x:COMPLEX, y:INT): COMPLEX -> _subComplexNumber;
+  //G _sub(x:COMPLEX, y:REAL): COMPLEX -> _subComplexNumber;
+  _subComplexNumber(x: Complex, y: number): Complex {
+    return new Complex(x.real - y, x.imag);
+  }
+
+  //G _mul(x:COMPLEX,y:COMPLEX):COMPLEX -> _mulComplex;
+  _mulComplex(x: Complex, y: Complex): Complex {
+    return Complex.mathjs2complex(
+      mathjs.multiply(x.toMathJs(), y.toMathJs()) as mathjs.Complex,
+    );
+  }
+
+  //G _div(x:COMPLEX,y:COMPLEX):COMPLEX -> _divComplex;
+  _divComplex(x: Complex, y: Complex): Complex {
+    return Complex.mathjs2complex(
+      mathjs.divide(x.toMathJs(), y.toMathJs()) as mathjs.Complex,
+    );
+  }
+
+  //G _mul(x:INT,y:COMPLEX):COMPLEX -> _mulNumberComplex;
+  //G _mul(x:REAL,y:COMPLEX):COMPLEX -> _mulNumberComplex;
+  _mulNumberComplex(x: number, y: Complex): Complex {
+    const z = y.clone();
+    z.mulReal(x);
+    return z;
+  }
+
+  //G _mul(x:COMPLEX,y:INT):COMPLEX -> _mulComplexNumber;
+  //G _mul(x:COMPLEX,y:REAL):COMPLEX -> _mulComplexNumber;
+  _mulComplexNumber(x: Complex, y: number): Complex {
+    const z = x.clone();
+    z.mulReal(y);
+    return z;
+  }
+
+  //G _pow(x:COMPLEX,y:COMPLEX):COMPLEX -> _powComplexComplex;
+  _powComplexComplex(x: Complex, y: Complex): Complex {
+    return Complex.mathjs2complex(
+      mathjs.pow(
+        Complex.complex2mathjs(x),
+        Complex.complex2mathjs(y),
+      ) as mathjs.Complex,
+    );
+  }
+
+  //G _pow(x:COMPLEX, y:INT):COMPLEX -> _powComplexNumber;
+  //G _pow(x:COMPLEX, y:REAL):COMPLEX -> _powComplexNumber;
+  _powComplexNumber(x: Complex, y: number): Complex {
+    return Complex.mathjs2complex(
+      mathjs.pow(Complex.complex2mathjs(x), y) as mathjs.Complex,
+    );
+  }
+
+  //G _pow(x:INT,y:COMPLEX):COMPLEX -> _powNumberComplex;
+  //G _pow(x:REAL,y:COMPLEX):COMPLEX -> _powNumberComplex;
+  _powNumberComplex(x: number, y: Complex): Complex {
+    return Complex.mathjs2complex(
+      mathjs.pow(x, Complex.complex2mathjs(y)) as mathjs.Complex,
+    );
+  }
+
+  //G conj(x:COMPLEX):COMPLEX -> _conjComplex;
+  _conjComplex(x: Complex): Complex {
+    return new Complex(x.real, -x.imag);
+  }
+
+  //G complex(x:INT,y:INT):COMPLEX -> _complex;
+  //G complex(x:INT,y:REAL):COMPLEX -> _complex;
+  //G complex(x:REAL,y:INT):COMPLEX -> _complex;
+  //G complex(x:REAL,y:REAL):COMPLEX -> _complex;
+  _complex(x: number, y: number): Complex {
+    return new Complex(x, y);
+  }
+
+  //G real(x:COMPLEX): REAL -> _extractComplexReal;
+  _extractComplexReal(x: Complex): number {
+    return x.real;
+  }
+
+  //G imag(x:COMPLEX): REAL -> _extractComplexImag;
+  _extractComplexImag(x: Complex): number {
+    return x.imag;
+  }
+
+  //G sqrtC(x:INT): COMPLEX -> _sqrtC_number;
+  //G sqrtC(x:REAL): COMPLEX -> _sqrtC_number;
+  _sqrtC_number(x: number): Complex {
+    if (x >= 0) return new Complex(Math.sqrt(x), 0);
+    else return new Complex(0, Math.sqrt(x));
+  }
+
+  //G sqrtC(x:COMPLEX): COMPLEX -> _sqrtC_complex;
+  _sqrtC_complex(x: Complex): Complex {
+    return Complex.mathjs2complex(
+      mathjs.sqrt(Complex.complex2mathjs(x)) as mathjs.Complex,
+    );
+  }
 
   //G abs(x:COMPLEX): REAL -> _absComplex;
   _absComplex(x: Complex): number {
-    //TODO return mathjs.abs(Complex.complex2mathjs(x));
-    return 0;
+    return Math.sqrt(x.real * x.real + x.imag * x.imag);
   }
 }
